97e668c5b5eeaf9694bdff6b3fb44546c57c1ba2
Johnathan <jonathanumana86@gmail.com>
2021-08-19 02:21:25 -0400

    Created home page with materail UI

--- e_commerce/src/App.js
+++ e_commerce/src/App.js
@@ -3,6 +3,7 @@ import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Products, Cart, Checkout } from './components';
 import { commerce } from './lib/commerce';
+import {Blog} from './components/Home/Home'
 import './App.css'
 
 const App = () => {
